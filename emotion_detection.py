2ead08848c1e1288b5f5b6560529fd7f44094dc2
rajusprof-collab <rajusprof@gmail.com>
2025-09-20 02:54:15 -0400

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,43 @@
+# import the requests library
+import requests
+
+# Add the function to take a text, do an emotion detection and return the results
+def emotion_detector(text_to_analyse):
+    '''
+    Takes a text as input, returns the emotion detector results
+    '''
+    # Define the URL for the emotion detector API
+    input_url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    input_json = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(input_url, json=input_json, headers=header)
+
+    return response.text
+
+
+
+
+
+'''
+import json
+    # Parse the response from the API and format into a proper json
+    formatted_response = json.loads(response.text)
+
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        label = None
+        score = None
+
+    # Return the label and score in a dictionary
+    return {'label': label, 'score': score}
+'''
